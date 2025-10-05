269a1709e8aba2c30a44a2f5726138b2a021b23b
ritu <irengomes018@gmail.com>
2025-10-05 23:34:20 +0600

    creating jwt

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"ecommerce/cmd"
-	"ecommerce/config"
+	"ecommerce/util"
 	"fmt"
 )
 
 func main() {
-	cnf := config.GetConfig()
+	// cnf := config.GetConfig()
 
-	fmt.Println(cnf.Version)
-	fmt.Println(cnf.ServiceName)
-	fmt.Println(cnf.HttpPort)
-	
-	cmd.Serve()
+	// fmt.Println(cnf.Version)
+	// fmt.Println(cnf.ServiceName)
+	// fmt.Println(cnf.HttpPort)
+
+	// cmd.Serve()
+	jwt, err := util.CreateJwt("my_secret", util.Payload{
+		Sub: 45,
+		FirstName: "Irene",
+		LastName: "Ritu",
+		Email: "ritzy333@gmail.com",
+		IsShopOwner: false,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(jwt)
+
 }
